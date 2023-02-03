d89bcb013e0afe697c57142348da01443afe09f6
jayanth197 <95013066+jayanth197@users.noreply.github.com>
2023-02-03 16:31:19 +0530

    Update build-partner.sh

--- jenkins/build-partner.sh
+++ jenkins/build-partner.sh
@@ -13,5 +13,5 @@ echo "Completed - copying file from $WARFilePath to $LocalWorkspace/target/$fold
 
 cd "$LocalWorkspace/target/"
 echo "Starting - Upload folder to S3 bucket"
-/usr/local/bin/aws s3 cp $foldername "s3://insureall/Builds/Partners/$foldername/" --recursive
+/usr/local/aws-cli/v2/2.4.6/dist/aws s3 cp $foldername "s3://insureall/Builds/Partners/$foldername/" --recursive
 echo "Completed - Upload folder to S3 bucket"
